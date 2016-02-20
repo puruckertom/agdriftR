a571eb64d3264fe42a7f4f70b0c44fca654d283e
Tom Purucker <purucker.tom@gmail.com>
2016-02-20 00:15:06 -0500

    reorg main r file

--- AgDriftR.R
+++ AgDriftR.R
@@ -1,5 +1,22 @@
 library(ggplot2)
 
+#carmen
+if(Sys.info()[4]=="carmens_computer_name"){
+  cdf.path <- "C://Users//ckuan//My Documents//AgDrift//"
+}
+#tom
+if(Sys.info()[4]=="stp-air-3.local" || Sys.info()[4]=="stp-air.local" || Sys.info()[4]=="stp-air"){
+  cdf.path <- "~/git/agdriftR/"
+}
+
+x <- 1:1300
+
+# AGDRIFT DATA (webversion) ####
+database <- read.csv(paste("agdrift_database.csv"), header = TRUE)
+for (n in 1:length(database)) {
+  assign(names(database)[n], database[[n]])
+}
+
 # AGDRIFT CALCULATIONS ####
 ## TIER I AERIAL
 
@@ -16,47 +33,47 @@ for (x in 1:1300) {
     ground_low_vf2f_90[x] <- 1.3322/(1.0 + 0.5262*x)^1.5548
   }
   # f2mc_90
-  while (x < 25) {
+  if(x < 25) {
     ground_low_f2mc_90 <- 1.0/(1.0 + 2.0913*x)^1.2572
-    else
+  else
       ground_low_f2mc_90 <- 0.1419/(1.0 + 0.3187*x)^1.3885
   }
   # vf2f_50
-  while (x < 25) {
+  if(x < 25) {
     ground_low_vf2f_50 <- 1.0/(1.0 + 0.8082*x)^1.5208
-    else
+  else
       ground_low_vf2f_50 <- 1.2628/(1.0 + 0.9749*x)^1.5085
   }
   #f2mc_50
-  while (x < 25) {
+  if(x < 25) {
     ground_low_f2mc_50 <- 1.0/(1.0 + 1.3742*x)^1.4863
-    else
+  else
       ground_low_f2mc_50 <- 1.2205/(1.0 + 1.6014*x)^1.4803
   }
   
   # High Boom -- D(x) = (c/(1+ax)^b) * (1+A exp(-Bx)) #exponential expression only for x > 25 ft
   #vf2f_90
-  while (x < 25) {
+  if(x < 25) {
     ground_high_vf2f_90 <- 1.0/(1.0 + 0.1243*x)^1.9100
-    else
+  else
       ground_high_vf2f_90 <- (1.3322/(1.0 + 0.5262*x)^1.5548) * (1.0 + 2.1749*exp(-0.001185*x))
   }
   #f2mc_90
-  while (x < 25) {
+  if(x < 25) {
     ground_high_f2mc_90 <- 1.0/(1.0 + 1.3058*x)^1.2714
-    else
+  else
       ground_high_f2mc_90 <- (0.1419/(1.0 + 0.3187*x)^1.3885) * (1.0 + 0.7089*exp(-0.000754*x))
   }
   #vf2f_50
-  while (x < 25) {
+  if(x < 25) {
     ground_high_vf2f_50 <- 1.0/(1.0 + 0.1409*x)^1.8605
-    else
+  else
       ground_high_vf2f_50 <- (1.2628/(1.0 + 0.9749*x)^1.5085) * (1.0 + 5.4877*exp(-0.001541*x))
   }
   #f2mc_50
-  while (x < 25) {
+  if(x < 25) {
     ground_high_f2mc_50 <- 1.0/(1.0 + 0.7802*x)^1.5183
-    else
+  else
       ground_high_f2mc_50 <- (1.2205/(1.0 + 1.6014*x)^1.4803) * (1.0 + 1.0639*exp(-0.000910*x))
   }
 }
@@ -74,9 +91,6 @@ airblast_orchard_outside <- 5.8017/(1.0 + 0.1183*x)^2.7872
 airblast_orchard_inside <- 0.3620/(1.0 + 1.0699*x)^1.3649
 
 
-# AGDRIFT DATA (webversion) ####
-database <- read.csv(paste("C://Users//ckuan//My Documents//AgDrift//agdrift_database.csv"), header = TRUE)
-for (n in 1:length(database)) {assign(names(database)[n], database[[n]])}
 
 # AGDRIFT PLOTTING ####
 ggplot(database, aes(x = database$distance, y = database$pond_ground_high_f2m)) +
