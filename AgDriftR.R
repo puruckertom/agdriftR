ad1f34626f44504018fa9d4eae544f75ed864d1e
Carmen Kuan <Ckuan@DZ2626UCKUAN.aa.ad.epa.gov>
2016-03-04 16:44:20 -0500

    plotting; Rmd to be updated

--- AgDriftR.R
+++ AgDriftR.R
@@ -13,22 +13,11 @@ if(Sys.info()[4]=="stp-air-3.local" || Sys.info()[4]=="stp-air.local" || Sys.inf
 
 # AGDRIFT DATA (webversion) ####
 df_python <- read.csv(paste(cdf.path, "agdrift_database.csv", sep = ""), header = TRUE)
-for (n in 1:length(df_python)) {
-  assign(names(df_python)[n], df_python[[n]])
-}
-
-#df_python$distance_m <- df_python$distance * 0.3048 #ft to meters
-
 
 # AGDRIFT DATA (worksheet calculator)
 df_aerial_excel <- read.xlsx(file = paste(cdf.path, "ubertool spray drift calculator_QC.xlsm", sep = ""), sheetName = "aerial depostion", startRow = 2, header = TRUE)
 df_ground_excel <- read.xlsx(file = paste(cdf.path, "ubertool spray drift calculator_QC.xlsm", sep = ""), sheetName = "ground deposition", startRow = 2, header = TRUE)
 df_airblast_excel <- read.xlsx(file = paste(cdf.path, "ubertool spray drift calculator_QC.xlsm", sep = ""), sheetName = "airblast deposition", startRow = 2, header = TRUE)
-
-# convert distance(ft) to distance(m)
-df_aerial_excel$distance_m <- round(df_aerial_excel$Distance..ft. / 3.28084)
-df_ground_excel$distance_m <- round(df_ground_excel$Distance..ft. / 3.28084)
-df_airblast_excel$distance_m <- round(df_airblast_excel$Distance..ft. / 3.28084)
 
 # AGDRIFT CALCULATIONS ####
 ## TIER I AERIAL
@@ -37,17 +26,17 @@ df_airblast_excel$distance_m <- round(df_airblast_excel$Distance..ft. / 3.28084)
 # Low Boom -- D(x) = c/(1+ax)^b 
 # x = downwind distance (ft)
 # x[1,1300] ft
-df_ground_calc <- data.frame(distance_ft = numeric(length = 1301),
-                             ground_low_vf2f_90 = numeric(1301),
-                             ground_low_f2mc_90 = numeric(1301),
-                             ground_low_vf2f_50 = numeric(1301),
-                             ground_low_f2mc_50 = numeric(1301),
-                             ground_high_vf2f_90 = numeric(1301),
-                             ground_high_f2mc_90 = numeric(1301),
-                             ground_high_vf2f_50 = numeric(1301),
-                             ground_high_f2mc_50 = numeric(1301), 
+df_ground_calc <- data.frame(distance_ft = numeric(length = 1001),
+                             ground_low_vf2f_90 = numeric(1001),
+                             ground_low_f2mc_90 = numeric(1001),
+                             ground_low_vf2f_50 = numeric(1001),
+                             ground_low_f2mc_50 = numeric(1001),
+                             ground_high_vf2f_90 = numeric(1001),
+                             ground_high_f2mc_90 = numeric(1001),
+                             ground_high_vf2f_50 = numeric(1001),
+                             ground_high_f2mc_50 = numeric(1001), 
                              row.names=NULL, stringsAsFactors=F)
-for (x in 0:1300) {
+for (x in 0:1000) {
   # distance_ft
   df_ground_calc$distance_ft[x+1] <- x
   # vf2f_90
@@ -71,19 +60,19 @@ for (x in 0:1300) {
 }
 
 ## TIER I ORCHARD/AIRBLAST -- D(x) = c/(1+ax)^b # x[1,600] ft
-df_airblast_calc <- data.frame(distance_ft = numeric(length = 601),
-                             airblast_normal_outside = numeric(length = 601),
-                             airblast_normal_inside = numeric(length = 601),
-                             airblast_dense_outside = numeric(length = 601),
-                             airblast_dense_inside = numeric(length = 601),
-                             airblast_sparse_outside = numeric(length = 601),
-                             airblast_sparse_inside = numeric(length = 601),
-                             airblast_vineyard_outside = numeric(length = 601),
-                             airblast_vineyard_inside = numeric(length = 601),
-                             airblast_orchard_outside = numeric(length = 601),
-                             airblast_orchard_inside = numeric(length = 601),
+df_airblast_calc <- data.frame(distance_ft = numeric(length = 1001),
+                             airblast_normal_outside = numeric(length = 1001),
+                             airblast_normal_inside = numeric(length = 1001),
+                             airblast_dense_outside = numeric(length = 1001),
+                             airblast_dense_inside = numeric(length = 1001),
+                             airblast_sparse_outside = numeric(length = 1001),
+                             airblast_sparse_inside = numeric(length = 1001),
+                             airblast_vineyard_outside = numeric(length = 1001),
+                             airblast_vineyard_inside = numeric(length = 1001),
+                             airblast_orchard_outside = numeric(length = 1001),
+                             airblast_orchard_inside = numeric(length = 1001),
                              row.names=NULL, stringsAsFactors=F)
-for (x in 0:600) {
+for (x in 0:1000) {
   # distance_ft
   df_airblast_calc$distance_ft[x+1] <- x
   df_airblast_calc$airblast_normal_outside[x+1] <- 0.9737/(1.0 + 1.0291*x)^1.9286
@@ -100,13 +89,74 @@ for (x in 0:600) {
 
 
 # AGDRIFT PLOTTING ####
-rownum <- c(1,9, 11:158)
-df_python_plot <- df_python[seq(1,300, by = 2),]
-df_aerial_excel_plot <- df_aerial_excel[1:150,]
-df_ground_excel_plot <- df_ground_excel[rownum,]
-df_airblast_excel_plot <- df_airblast_excel[rownum,]
+# Aerial
+## Very Fine to Fine
+par(mfrow=c(4,1))
+plot(df_python$distance_ft, df_python$pond_aerial_vf2f, main = "vf2f", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition",xlim=c(0,600), ylim=c(0, 0.55))
+lines(df_aerial_excel$Distance..ft., df_aerial_excel$Very.Fine.to.Fine, lty=2)
+legend("topleft", c("Python", "Peck"), col = c("black", "black"), lty = c(1, 2), bg = "gray90", cex=0.75)
+## Fine to medium
+plot(df_python$distance_ft, df_python$pond_aerial_f2m, col = "red", main = "f2m", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition",xlim=c(0,600), ylim=c(0, 0.55))
+lines(df_aerial_excel$Distance..ft., df_aerial_excel$Fine.to.Med, col = "red", lty=2)
+legend("topleft", c("Python", "Peck"), col = c("red", "red"), lty = c(1, 2), bg = "gray90", cex=0.75)
+## Medium to coarse
+plot(df_python$distance_ft, df_python$pond_aerial_m2c, col = "green3", type = "l", main = "m2c", xlab = "Distance (ft)", ylab = "Fraction of Deposition",xlim=c(0,600), ylim=c(0, 0.55))
+lines(df_aerial_excel$Distance..ft., df_aerial_excel$Med.to.Coarse, col = "green3", lty=2)
+legend("topleft", c("Python", "Peck"), col = c("green3", "green3"), lty = c(1, 2), bg = "gray90", cex=0.75)
+## Coarse to very coarse
+plot(df_python$distance_ft, df_python$pond_aerial_c2vc, col = "blue", type = "l", main = "c2vc", xlab = "Distance (ft)", ylab = "Fraction of Deposition",xlim=c(0,600), ylim=c(0, 0.55))
+lines(df_aerial_excel$Distance..ft., df_aerial_excel$Coarse.to.VC, col = "blue", lty=2)
+legend("topleft", c("Python", "Peck"), col = c("blue", "blue"), lty = c(1, 2), bg = "gray90", cex=0.75)
 
-ggplot(df_python, aes(x=df_python$distance_ft)) +
-  geom_line(aes(y=df_python$pond_airblast_orchard, colour = "var0")) +
-  #geom_line(aes(y=df_airblast_calc$airblast_orchard_outside[1:300], colour = "var1")) +
-  geom_line(aes(y=df_airblast_calc$airblast_orchard_inside[1:300], colour = "var2"))
+# Ground
+## Low- very fine to fine
+par(mfrow=c(1,5))
+plot(df_ground_calc$distance_ft, df_ground_calc$ground_low_vf2f_90, main = "Low vf2f", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition",xlim=c(0,300))
+lines(df_ground_excel$Distance..ft., df_ground_excel$Low.boom..vf, lty=2)
+lines(df_python$distance_ft, df_python$pond_ground_low_vf2f, lty=4)
+legend("topleft", c("Calc", "Peck", "Python"), col = c("black", "black", "black"), lty = c(1, 2, 4), bg = "gray90", cex = 0.75)
+## Low- fine to medium coarse
+plot(df_ground_calc$distance_ft, df_ground_calc$ground_low_f2mc_90, col = "red", main = "Low f2mc", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition",xlim=c(0,300))
+lines(df_ground_excel$Distance..ft., df_ground_excel$low.boom..fmc, col = "red", lty=2)
+lines(df_python$distance_ft, df_python$pond_ground_low_f2m, col = "red", lty=4)
+legend("topleft", c("Calc", "Peck", "Python"), col = c("red", "red", "red"), lty = c(1, 2, 4), bg = "gray90", cex = 0.75)
+## High- very fine to fine
+plot(df_ground_calc$distance_ft, df_ground_calc$ground_high_vf2f_90, col = "green3", main = "High Vf2f", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition",xlim=c(0,300))
+lines(df_ground_excel$Distance..ft., df_ground_excel$High.boom..vf, col = "green3", lty=2)
+lines(df_python$distance_ft, df_python$pond_ground_high_vf2f, col = "green3", lty=4)
+legend("topleft", c("Calc", "Peck", "Python"), col = c("green3", "green3", "green3"), lty = c(1, 2, 4), bg = "gray90", cex = 0.75)
+## High- fine to medium coarse
+plot(df_ground_calc$distance_ft, df_ground_calc$ground_high_f2mc_90, col = "blue", main = "High f2mc", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition",xlim=c(0,300))
+lines(df_ground_excel$Distance..ft., df_ground_excel$high.boom..fmc, col = "blue", lty=2)
+lines(df_python$distance_ft, df_python$pond_ground_high_f2m, col = "blue", lty=4)
+legend("topleft", c("Calc", "Peck", "Python"), col = c("blue", "blue", "blue"), lty = c(1, 2, 4), bg = "gray90", cex = 0.75)
+
+# Airblast
+## orchard
+par(mfrow=c(1,5))
+plot(df_airblast_calc$distance_ft, df_airblast_calc$airblast_orchard_inside, main = "Orchard", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition", xlim=c(0,600))
+lines(df_airblast_calc$distance_ft, df_airblast_calc$airblast_orchard_outside, col="blue", lty=2)
+lines(df_python$distance_ft, df_python$pond_airblast_orchard, col = "red", lty=2)
+lines(df_airblast_excel$Distance..ft., df_airblast_excel$Orchard, col = "green3", lty=2)
+legend("topleft", c("Calc- inside", "Calc- outside", "Python", "Peck"), col = c("black", "blue", "red", "green3"), lty = c(1, 2, 2, 2), bg = "gray90", cex=0.75)
+## Vineyard
+plot(df_airblast_calc$distance_ft, df_airblast_calc$airblast_vineyard_inside, main = "Vineyard", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition", xlim=c(0,600))
+lines(df_airblast_calc$distance_ft, df_airblast_calc$airblast_vineyard_outside, col="blue", lty=2)
+lines(df_python$distance_ft, df_python$pond_airblast_vineyard, col = "red", lty=2)
+lines(df_airblast_excel$Distance..ft., df_airblast_excel$Vineyard, col = "green3", lty=2)
+legend("topleft", c("Calc- inside", "Calc- outside", "Python", "Peck"), col = c("black", "blue", "red", "green3"), lty = c(1, 2, 2, 2), bg = "gray90", cex=0.75)
+## Normal
+plot(df_airblast_calc$distance_ft, df_airblast_calc$airblast_normal_inside, main = "Normal", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition", xlim=c(0,600))
+lines(df_airblast_calc$distance_ft, df_airblast_calc$airblast_normal_outside, col="blue", lty=2)
+lines(df_airblast_excel$Distance..ft., df_airblast_excel$Normal, col = "green3", lty=2)
+legend("topleft", c("Calc- inside", "Calc- outside", "Peck"), col = c("black", "blue", "green3"), lty = c(1, 2, 2), bg = "gray90", cex = 0.75)
+## Sparse
+plot(df_airblast_calc$distance_ft, df_airblast_calc$airblast_sparse_inside, main = "Sparse", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition", xlim=c(0,600))
+lines(df_airblast_calc$distance_ft, df_airblast_calc$airblast_sparse_outside, col="blue", lty=2)
+lines(df_airblast_excel$Distance..ft., df_airblast_excel$Sparse, col = "green3", lty=2)
+legend("topleft", c("Calc- inside", "Calc- outside", "Peck"), col = c("black", "blue", "green3"), lty = c(1, 2, 2), bg = "gray90", cex = 0.75)
+## Dense
+plot(df_airblast_calc$distance_ft, df_airblast_calc$airblast_dense_inside, main = "Dense", type = "l", xlab = "Distance (ft)", ylab = "Fraction of Deposition", xlim=c(0,600))
+lines(df_airblast_calc$distance_ft, df_airblast_calc$airblast_dense_outside, col="blue", lty=2)
+lines(df_airblast_excel$Distance..ft., df_airblast_excel$Dense, col = "green3", lty=2)
+legend("topleft", c("Calc- inside", "Calc- outside", "Peck"), col = c("black", "blue", "green3"), lty = c(1, 2, 2), bg = "gray90", cex = 0.75)
